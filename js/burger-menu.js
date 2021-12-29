59529621315e39306b96fa9ece8dea7d50d9d162
Jamsen <yiann.sky@gmail.com>
2021-12-29 18:00:47 +0100

    add overflow hidden for body

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,12 +1,15 @@
 const hamburger = document.querySelector(".header__hamburger");
 const navMenu = document.querySelector(".header__nav-menu");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  body.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
+  body.classList.remove("active");
 }))
