678fc826d3feee8745d3df5f6358aec8fc19147f
OlenaTyminska <to_v@ukr.net>
2022-01-05 00:20:07 +0000

    correct carousel

--- js/our_honey-carousel.js
+++ js/our_honey-carousel.js
@@ -1,41 +1,72 @@
 (function() {
-
-    const slides = [
-        '<div><img src="img/w-f-honey.jpg" alt="wildflower honey"></div>',
-        '<div><img src="img/bl-honey.jpg" alt="blossom honey"></div>',
-        '<div><img src="img/cl-honey.jpg" alt="clover honey"></div>',
-        '<div><img src="img/bw-honey.jpg" alt="buckwheat honey"></div>',
-        '<div><img src="img/c-b-honey.jpg" alt="citrus blossom-honey"></div>'
-    ];
+    const jars = [
+        {
+          title: "Wildflower Honey",
+          image: "img/w-f-honey.jpg",
+          price: "$7.00",
+        },
+        {
+          title: "Blossom Honey",
+          image: "img/bl-honey.jpg",
+          price: "$20.00",
+        },
+        {
+          title: "Clover Honey",
+          image: "img/cl-honey.jpg",
+          price: "$8.00",
+        },
+        {
+          title: "Buckwheat Honey",
+          image: "img/bw-honey.jpg",
+          price: "$10.00",
+        },
+        {
+          title: "Citrus Blossom Honey",
+          image: "img/c-b-honey.jpg",
+          price: "$15.00",
+        },
+      ];
+    
+      function renderSlide(jars) {
+        return `<article class="our_honey-show__card">
+          <img class="our_honey-show__image" width="300" src="${jars.image}" alt="${jars.title}">
+          <h3 class="our_honey-show__name">${jars.title}</h3>
+          <p class="our_honey-show__price">${jars.price}</p>
+          <button class="our_honey-button">Buy Now</button>
+        </article>`;
+        }
 
     let currentSlide = 0;
 
-    function showCurrentSlide() {
-        const slideContainer = document.querySelector(".our_honey-carousel__slide");
-        slideContainer.innerHTML = slides[currentSlide];
+    function renderJars() {
+        const jarsContainer = document.querySelector(".our_honey-carousel__slide");
+        jarsContainer.innerHTML = renderSlide(jars[currentSlide]);
         if (window.innerWidth > 600) {
-            const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
-            slideContainer.innerHTML += slides[secondSlide];
+            const secondSlide =
+                currentSlide + 1 >= jars.length ? 0 : currentSlide + 1;
+            jarsContainer.innerHTML += renderSlide(jars[secondSlide]);
+
             if (window.innerWidth > 900) {
-                const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1;
-                slideContainer.innerHTML += slides[thirdSlide];
+                const thirdSlide =
+                secondSlide + 1 >= jars.length ? 0 : secondSlide + 1;
+                jarsContainer.innerHTML += renderSlide(jars[thirdSlide]);
             }
         }
     }
 
     function nextSlide() {
-        currentSlide = currentSlide +1 >= slides.length ? 0 : currentSlide + 1;
-        showCurrentSlide();
+        currentSlide = currentSlide +1 >= jars.length ? 0 : currentSlide + 1;
+        renderJars();
     }
+
+    renderJars(jars);
 
     function prevSlide() {
-        currentSlide = currentSlide - 1 <= 0 ? slides.length - 1 : currentSlide - 1;
-        showCurrentSlide();
+        currentSlide = currentSlide - 1 <= 0 ? jars.length - 1 : currentSlide - 1;
+        renderJars();
     }
-
-    showCurrentSlide();
 
     document.querySelector('.our_honey-carousel__button.forward').addEventListener('click', nextSlide);
     document.querySelector('.our_honey-carousel__button.back').addEventListener('click', prevSlide);
-        window.addEventListener('resize', showCurrentSlide);
+    window.addEventListener('resize', renderJars);
 })();
